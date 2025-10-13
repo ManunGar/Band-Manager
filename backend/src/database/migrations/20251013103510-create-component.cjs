b95bfb46c0291d4ab825d111f4d15f36f8cd75d1
ManunGar <nunogarciamanuel@gmail.com>
2025-10-13 14:51:07 +0200

    feat: Add Component model and migration for database

---
+++ backend/src/database/migrations/20251013103510-create-component.cjs
@@ -0,0 +1,86 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Components', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      musicianId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Musicians',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      bandId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Bands',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      private: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      administrator: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      }
+    });
+    await queryInterface.createTable('ComponentInstruments', {
+      componentId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Components',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      instrumentId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Instruments',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('InstrumentComponents');
+    await queryInterface.dropTable('Components');
+  }
+};

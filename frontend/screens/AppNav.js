184426c94fe1244dcd718f8fd8a860eaeb834c89
ManunGar <nunogarciamanuel@gmail.com>
2025-10-18 17:03:03 +0200

    feat: Load custom font BebasNeue in AppNav component

--- frontend/screens/AppNav.js
+++ frontend/screens/AppNav.js
@@ -1,12 +1,20 @@
+import { BebasNeue_400Regular } from '@expo-google-fonts/bebas-neue'
 import { NavigationContainer } from '@react-navigation/native'
+import { useFonts } from 'expo-font'
 import AuthStack from './auth/AuthStack'
 
 const AppNav = () => {
-  return (
-    <NavigationContainer>
-        <AuthStack />
-    </NavigationContainer>
-  )
+
+    const [fontsLoaded] = useFonts({
+        BebasNeue_400Regular,
+    });
+
+
+    return (
+        <NavigationContainer>
+            <AuthStack />
+        </NavigationContainer>
+    )
 }
 
 export default AppNav

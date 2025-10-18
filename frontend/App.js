3548d760af90d7c30c90dcc6e6d45ca63c71e206
ManunGar <nunogarciamanuel@gmail.com>
2025-10-18 17:02:53 +0200

    feat: Implement SafeAreaProvider and SafeAreaView in App component

--- frontend/App.js
+++ frontend/App.js
@@ -1,4 +1,5 @@
 import { createStackNavigator } from '@react-navigation/stack';
+import { initialWindowMetrics, SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import AppNav from './screens/AppNav';
 
 const Stack = createStackNavigator();
@@ -6,7 +7,11 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <>
-      <AppNav />
+      <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+        <SafeAreaView style={{ flex: 1 }} edges={['top', 'left', 'right']}>
+          <AppNav />
+        </SafeAreaView>
+      </SafeAreaProvider>
     </>
   );
 }

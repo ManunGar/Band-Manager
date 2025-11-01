6c2d113169f7515b11631de3d4a78cc9f5c9d18b
ManunGar <nunogarciamanuel@gmail.com>
2025-11-01 19:42:56 +0100

    feat: Add InstrumentLevel component to display instrument details

---
+++ frontend/components/InstrumentLevel.jsx
@@ -0,0 +1,44 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+import * as GlobalStyle from '../GlobalStyle'
+import LinkText from './LinkText'
+
+const InstrumentLevel = ({ instrument }) => {
+    
+    return (
+        <View style={styles.container}>
+            <View style={{ flexDirection: 'row', alignItems: 'flex-end', gap: 10 }}>
+                <Image
+                    source={{ uri: `${process.env.EXPO_PUBLIC_API_URL}${instrument.image}` }}
+                    style={{ width: 40, height: 40 }}
+                />
+                <View>
+                    <Text style={styles.name}>{instrument.name}</Text>
+                    <Text style={styles.level}>{instrument.MusicianLevel.level}</Text>
+                </View>
+            </View>
+            <LinkText>Editar</LinkText>
+        </View>
+    )
+}
+
+export default InstrumentLevel
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    name: {
+        fontFamily: 'BebasNeue',
+        fontSize: 24,
+        color: GlobalStyle.darkGray,
+        textTransform: 'uppercase',
+    },
+    level: {
+        fontFamily: 'Oswald_400',
+        fontSize: 16,
+        color: GlobalStyle.black,
+        textTransform: 'capitalize'
+    }
+})

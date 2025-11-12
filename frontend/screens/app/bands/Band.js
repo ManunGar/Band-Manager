9d91c4b77dc68aa659b6018262ab59031548b0a7
ManunGar <nunogarciamanuel@gmail.com>
2025-11-12 13:34:03 +0100

    feat: Implement functionality to fetch and display user's bands

--- frontend/screens/app/bands/Band.js
+++ frontend/screens/app/bands/Band.js
@@ -1,18 +1,61 @@
-import { useContext } from 'react'
-import { Pressable, Text, View } from 'react-native'
+import { useFocusEffect } from '@react-navigation/native'
+import { useCallback, useContext, useState } from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import BandEndpoints from '../../../api/BandEndpoints'
+import TopContainer from '../../../components/TopContainer'
 import { AuthContext } from '../../../contexts/AuthContext'
 
 const Band = () => {
+    const [bands, setBands] = useState([])
+    const { user } = useContext(AuthContext)
 
-    const {logout} = useContext(AuthContext)
+    useFocusEffect(
+        useCallback(() => {
+            // Refresh bands when returning to the screen
+            fetchBands();
+        }, [])
+    );
+
+    const fetchBands = async () => {
+        try {
+            const bands = await BandEndpoints.listMyBands();
+            setBands(bands.bands);
+        } catch (error) {
+            console.error("Error fetching bands:", error);
+        }
+    }
 
     return (
         <View>
-            <Pressable onPress={logout}>
-                <Text>Logout</Text>
-            </Pressable>
+            <TopContainer
+                backEnabled={false}
+                editEnabled={false}
+                style={{ paddingBottom: 24, paddingTop: 24, alignItems: 'none' }}>
+                <Text style={styles.title}>Bandas</Text>
+            </TopContainer>
+            <FlatList
+                data={bands}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => (
+                    <Text>{item.name}</Text>
+                )}
+                ListEmptyComponent={<Text style={styles.noBandsText}>No perteneces a ninguna banda</Text>}
+                contentContainerStyle={{ alignItems: 'center' }}
+            />
         </View>
     )
 }
 
 export default Band
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 40,
+        fontFamily: 'BebasNeue'
+    },
+    noBandsText: {
+        fontFamily: 'Oswald_400',
+        fontSize: 16,
+        color: 'gray'
+    }
+})

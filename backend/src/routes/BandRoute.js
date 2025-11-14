3192004581dec6e7232e5e86b3c063b2d80691ea
ManunGar <nunogarciamanuel@gmail.com>
2025-11-14 13:25:17 +0100

    feat: Implement findBandById functionality

--- backend/src/routes/BandRoute.js
+++ backend/src/routes/BandRoute.js
@@ -1,5 +1,6 @@
 import BandController from "../controllers/BandController.js"
 import { isLoggedIn } from "../middleware/AuthMiddleware.js"
+import { isBandMember } from "../middleware/BandMiddleware.js"
 import * as BandValidation from "../validations/BandValidation.js"
 import { handleValidation } from '../validations/HandleValidation.js'
 
@@ -17,6 +18,13 @@ const loadFileRoutes = function (app) {
             BandValidation.create,
             handleValidation,
             BandController.createBand
+        )
+    // Get, update, delete band routes
+    app.route('/bands/:bandId')
+        .get(
+            isLoggedIn,
+            isBandMember, // Ensure the user is a member of the band
+            BandController.findBandById
         )
 }
 

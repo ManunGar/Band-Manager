e659a7917fbe81f45fbf0249f1f1910925f93c93
ManunGar <nunogarciamanuel@gmail.com>
2025-11-14 13:25:08 +0100

    feat: Add middleware to check if a musician is a member of a band

---
+++ backend/src/middleware/BandMiddleware.js
@@ -0,0 +1,25 @@
+import { Component } from "../models/sequelize.js";
+
+// Middleware to check if the musician is a member of the band
+const isBandMember = async (req, res, next) => {
+    const bandId = req.params.bandId;
+    const musicianId = req.user.musician.id;
+    console.log("🚀 ~ isBandMember ~ musicianId:", musicianId)
+    try {
+        const component = await Component.findOne({
+            where: {
+                bandId: bandId,
+                musicianId: musicianId
+            }
+        });
+        if (!component) {
+            return res.status(403).send({ error: 'Access denied. You are not a member of this band.' });
+        }
+        next();
+    } catch (error) {
+        console.error('Error checking band membership:', error);
+        res.status(500).send({ error: 'Error checking band membership' });
+    }
+};
+
+export { isBandMember };

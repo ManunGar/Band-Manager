fc20ad830756679a16624e38fbc2c499d39d4e56
ManunGar <nunogarciamanuel@gmail.com>
2025-11-17 20:59:33 +0100

    feat: Implement BandScreen to display band details and fetch data from API

--- frontend/screens/app/bands/BandScreen.js
+++ frontend/screens/app/bands/BandScreen.js
@@ -1,11 +1,51 @@
-import { Text, View } from 'react-native'
+import { useEffect, useState } from 'react'
+import { Image, Text, View } from 'react-native'
+import BandEndpoints from '../../../api/BandEndpoints'
+import TopContainer from '../../../components/TopContainer'
+import * as GlobalStyle from '../../../GlobalStyle'
 
-const BandScreen = () => {
-  return (
-    <View>
-      <Text> Band Screen </Text>
-    </View>
-  )
+const BandScreen = ({ route }) => {
+    const [band, setBand] = useState(null)
+    const { bandId } = route.params
+
+    useEffect(() => {
+        fetchBandDetails();
+    }, [])
+
+    const fetchBandDetails = async () => {
+        try {
+            const data = await BandEndpoints.getBandDetails(bandId)
+            console.log(data)
+            setBand(data?.band)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchBandDetails();
+    }, [])
+
+    return (
+        <View>
+            <TopContainer
+                editEnabled={true} // TODO: Verify admin permissions
+                createEnabled={true}
+                style={{paddingTop: 5}}>
+                <Image source={{ uri: band?.profile_picture }} style={{ width: 70, height: 70 }} />
+                <Text style={{ fontSize: 26, fontFamily: 'BebasNeue', marginTop: 5 }}>
+                    {band?.name}
+                </Text>
+                <Text style={{ fontSize: 12, fontFamily: 'Oswald_500', color: GlobalStyle.yellow, textTransform: 'uppercase', textAlign: 'center' }}>
+                    {band?.type}
+                </Text>
+            </TopContainer>
+            {/* BAND HOME */}
+            <View>
+
+            </View>
+        </View>
+    )
 }
 
 export default BandScreen

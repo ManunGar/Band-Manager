42975cc22c71733c908ed8d78ae09cae8bcb8a55
ManunGar <nunogarciamanuel@gmail.com>
2025-12-31 10:44:54 +0100

    feat: add update and delete functionality for bands, including profile picture management

--- backend/src/routes/BandRoute.js
+++ backend/src/routes/BandRoute.js
@@ -1,6 +1,6 @@
 import BandController from "../controllers/BandController.js"
 import { isLoggedIn } from "../middleware/AuthMiddleware.js"
-import { isBandMember, isNotBandMember } from "../middleware/BandMiddleware.js"
+import { isBandAdmin, isBandMember, isNotBandMember } from "../middleware/BandMiddleware.js"
 import * as BandValidation from "../validations/BandValidation.js"
 import { handleValidation } from '../validations/HandleValidation.js'
 
@@ -25,6 +25,18 @@ const loadFileRoutes = function (app) {
             isLoggedIn,
             isBandMember, // Ensure the user is a member of the band
             BandController.findBandById
+        )
+        .put(
+            isLoggedIn,
+            isBandAdmin, // Ensure the user is an admin of the band
+            BandValidation.update,
+            handleValidation,
+            BandController.updateBand
+        )
+        .delete(
+            isLoggedIn,
+            isBandAdmin, // Ensure the user is an admin of the band
+            BandController.deleteBand
         )
     // Get by code route
     app.route('/bands/code/:bandCode')

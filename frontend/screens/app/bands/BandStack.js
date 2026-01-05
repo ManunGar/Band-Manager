01dd6d321b58e47512bd7a0b11a35c4653b3f292
ManunGar <nunogarciamanuel@gmail.com>
2026-01-05 12:26:08 +0100

    feat: add EditComponentScreen to BandStack navigator

--- frontend/screens/app/bands/BandStack.js
+++ frontend/screens/app/bands/BandStack.js
@@ -2,6 +2,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { useContext } from 'react';
 import { AuthContext } from '../../../contexts/AuthContext';
 import ComponentScreen from '../component/ComponentScreen';
+import EditComponentScreen from '../component/EditComponentScreen';
 import BandCreate from './BandCreate';
 import BandScreen from './BandScreen';
 import BandsScreen from './BandsScreen';
@@ -22,7 +23,9 @@ const Bands = () => {
             <Stack.Screen name="BandDetails" component={BandScreen} />
             <Stack.Screen name="CreateBand" component={BandCreate} />
             <Stack.Screen name="JoinBand" component={JoinBandScreen} />
+            {/* COMPONENT */}
             <Stack.Screen name="Component" component={ComponentScreen} />
+            <Stack.Screen name="EditComponent" component={EditComponentScreen} />
         </Stack.Navigator>
     )
 }

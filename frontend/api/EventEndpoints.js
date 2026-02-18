c8a41a1cf5da1251dfc2d44e4c33013bfa1d06b9
ManunGar <nunogarciamanuel@gmail.com>
2026-02-18 14:11:32 +0100

    feat: add EventEndpoints for listing events via API

---
+++ frontend/api/EventEndpoints.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { handleError } from "./handleError.js";
+
+const baseUrl = process.env.EXPO_PUBLIC_API_URL; // Adjust the base URL as needed
+
+// Endpoint to list events
+const listEvents = async (bandId, timeScope, type) => {
+    try {
+        const response = await axios.get(`${baseUrl}/events?bandId=${bandId}&timeScope=${timeScope}&type=${type}`);
+        return response.data;
+    } catch (error) {
+        handleError(error);
+    }
+};
+
+export default {
+    listEvents,
+}

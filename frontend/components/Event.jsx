92c4ff869e7a4bd3e3d81e343ab185d5950e5c08
ManunGar <nunogarciamanuel@gmail.com>
2026-02-19 16:27:45 +0100

    feat: add band profile picture and attendance status to event display

--- frontend/components/Event.jsx
+++ frontend/components/Event.jsx
@@ -1,4 +1,5 @@
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import bandProfilePictureDefault from '../assets/milestones/band_default.png';
 import performanceDefaultImage from '../assets/milestones/performance_default.jpg';
 import rehearsalDefaultImage from '../assets/milestones/rehearsal_default.jpg';
 import * as GlobalStyle from '../GlobalStyle';
@@ -8,8 +9,16 @@ const Event = ({ event }) => {
     return (
         <TouchableOpacity>
             <Image source={event.Performance?.picture ? { uri: `${event.Performance.picture}` } : event.Performance ? performanceDefaultImage : rehearsalDefaultImage} style={{ width: 385, height: 180, borderRadius: 10 }} />
+            <Image source={event.band.profile_picture ? { uri: `${event.band.profile_picture}` } : bandProfilePictureDefault} style={styles.bandPicture } />
             <View>
-                <Text style={styles.title} numberOfLines={1} ellipsizeMode="tail">{event.Performance?.name || 'Ensayo'}</Text>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'baseline', width: 385 }}>
+                    <Text style={styles.title} numberOfLines={1} ellipsizeMode="tail">{event.Performance?.name || 'Ensayo'}</Text>
+                    <Text style={{ fontFamily: 'Oswald_400', fontSize: 16, marginLeft: 10, flexShrink: 0, color: event.attendance.present === true ? GlobalStyle.darkGreen : event.attendance.present === false ? GlobalStyle.darkRed : GlobalStyle.gray }}>
+                        { event.attendance.present && 'Asistencia Confirmada' }
+                        { event.attendance.present === false && 'Asistencia Rechazada' }
+                        { event.attendance.present === null && 'Sin Confirmar' }
+                    </Text>
+                </View>
                 <Text style={styles.subtitle} numberOfLines={1} ellipsizeMode="tail">{parseDateTime(event.date, event.initialTime)} {event.Performance?.place ? `· ${event.Performance.place}` : ''}</Text>
             </View>
         </TouchableOpacity>
@@ -18,12 +27,21 @@ const Event = ({ event }) => {
 
 export default Event
 const styles = StyleSheet.create({
+    bandPicture: {
+        width: 50,
+        height: 50,
+        borderRadius: 5,
+        position: 'absolute',
+        top: 10,
+        left: 10,
+        backgroundColor: GlobalStyle.white,
+    },  
     title: {
         fontFamily: 'BebasNeue',
         fontSize: 24,
         color: GlobalStyle.black,
-        width: 280,
         textTransform: 'uppercase',
+        flexShrink: 1,
     },
     subtitle: {
         fontFamily: 'Oswald_400',

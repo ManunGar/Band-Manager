cc49a9af3ceb894596658723ab2515ccb26a913a
ManunGar <nunogarciamanuel@gmail.com>
2026-02-23 19:34:05 +0100

    feat: add android status bar and navigation bar configurations

--- frontend/App.js
+++ frontend/App.js
@@ -1,5 +1,8 @@
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { createStackNavigator } from '@react-navigation/stack';
+import * as NavigationBar from "expo-navigation-bar";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 import 'react-native-gesture-handler';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { initialWindowMetrics, SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
@@ -9,10 +12,17 @@ import AppNav from './screens/AppNav';
 const Stack = createStackNavigator();
 
 export default function App() {
+
+  useEffect(() => {
+    NavigationBar.setBackgroundColorAsync("#FFFFFF");
+    NavigationBar.setButtonStyleAsync("dark");
+  }, []);
+
   return (
     <>
       <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-        <SafeAreaView style={{ flex: 1 }} edges={['top', 'left', 'right', 'bottom']}>
+        <StatusBar style="dark" backgroundColor="#FFFFFF" />
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#FFFFFF' }} edges={['top', 'left', 'right', 'bottom']} >
           <GestureHandlerRootView style={{ flex: 1 }}>
             <BottomSheetModalProvider>
               <AuthProvider>

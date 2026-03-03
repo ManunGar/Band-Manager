d95c7b15313bcdec8334c61bbe6872229fc367c1
ManunGar <nunogarciamanuel@gmail.com>
2026-03-03 12:53:27 +0100

    fix: correct height property in NoConfirmIcon component

--- frontend/components/icons/NoConfirmIcon.jsx
+++ frontend/components/icons/NoConfirmIcon.jsx
@@ -4,7 +4,7 @@ function NoConfirmIcon({ width, height, stroke, strokeWidth, fill }, props) {
   return (
     <Svg
       width={width || 21}
-      height={height || 23}
+      height={height || 21}
       viewBox="0 0 21 23"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"

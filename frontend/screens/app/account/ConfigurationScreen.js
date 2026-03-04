e445d0580a86618d5cecc7d9305eabc8e1b9c80f
ManunGar <nunogarciamanuel@gmail.com>
2026-03-04 11:43:18 +0100

    feat: improve layout and structure of ConfigurationScreen for better usability

--- frontend/screens/app/account/ConfigurationScreen.js
+++ frontend/screens/app/account/ConfigurationScreen.js
@@ -10,29 +10,47 @@ const ConfigurationScreen = ({ route }) => {
     const { logout } = useContext(AuthContext)
     const { musician } = route.params;
 
-  return (
-    <View style={{ flex: 1 }}>
-        <TopContainer backEnabled title={'Configuración'} editEnabled={false}
-            style={{ alignItems: 'flex-start' }}>
-            <View style={styles.profileImageContainer}>
-                <Image 
-                    source={musician?.profile_picture ? { uri: musician.profile_picture } : profileDefault}
-                    style={styles.profileImage}
-                />
-                <View style={ styles.musicianInfoContainer }>
-                    <Text style={styles.nameText}>{musician?.full_name}</Text>
-                    <Text style={styles.usernameText}>@{musician?.username}</Text>
-                    <LinkText>Editar Perfil</LinkText>
+    return (
+        <View style={{ flex: 1 }}>
+            <TopContainer backEnabled title={'Configuración'} editEnabled={false}
+                style={{ alignItems: 'flex-start' }}>
+                <View style={styles.profileImageContainer}>
+                    <Image
+                        source={musician?.profile_picture ? { uri: musician.profile_picture } : profileDefault}
+                        style={styles.profileImage}
+                    />
+                    <View style={styles.musicianInfoContainer}>
+                        <Text style={styles.nameText}>{musician?.full_name}</Text>
+                        <Text style={styles.usernameText}>@{musician?.username}</Text>
+                        <LinkText>Editar Perfil</LinkText>
+                    </View>
+                </View>
+            </TopContainer>
+            <View style={styles.bodyContainer}>
+                <Text style={styles.sectionTitle}>Cuenta</Text>
+                <View style={styles.sectionContainer}>
+                    <Text style={styles.logoutText} onPress={logout}>Cerrar sesión</Text>
+                </View>
+                <Text style={styles.sectionTitle}>Aplicación</Text>
+                <View style={styles.sectionContainer}>
+                    <View style={styles.subSectionContainer}>
+                        <Text style={styles.subSectionTitle}>Tema</Text>
+                        <LinkText>Claro</LinkText>
+                    </View>
+                    <View>
+                        <Text style={styles.subSectionTitle}>Idioma</Text>
+                        <LinkText>Español</LinkText>
+                    </View>
+                </View>
+                <Text style={styles.sectionTitle}>Ayuda</Text>
+                <View style={styles.sectionContainer}>
+                    <View style={styles.subSectionContainer}>
+                        <Text style={styles.subSectionTitle}>Sobre la aplicación</Text>
+                        <Text style={styles.subSectionText}>Versión 0.1.1</Text>
+                    </View>
                 </View>
             </View>
-        </TopContainer>
-      <View style={styles.bodyContainer}>
-        <Text style={styles.sectionTitle}>Cuenta</Text>
-        <View style={styles.sectionContainer}>
-            <Text style={styles.logoutText} onPress={logout}>Cerrar sesión</Text>
         </View>
-      </View>
-    </View>
     )
 }
 
@@ -74,11 +92,25 @@ const styles = StyleSheet.create({
         marginBottom: 15
     },
     sectionContainer: {
-        paddingInline: 10
+        paddingInline: 10,
+        marginBottom: 40,
+        gap: 15,
+    },
+    subSectionContainer: {
+        flexDirection: 'column',
+    },
+    subSectionTitle: {
+        fontFamily: 'Oswald_400',
+        fontSize: 18,
+    },
+    subSectionText: {
+        fontFamily: 'Oswald_400',
+        fontSize: 15,
+        color: GlobalStyle.gray
     },
     logoutText: {
         fontFamily: 'Oswald_400',
-        fontSize: 16,
+        fontSize: 18,
         color: GlobalStyle.red,
     }
 })
